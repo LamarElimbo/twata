92ef3269aa53c8e83e32a5ed2e9eba41821b2299
lamar133 <lamar_johnson133@yahoo.ca>
2017-11-16 23:58:33 -0500

    settings setup

--- settings.py
+++ settings.py
@@ -11,6 +11,7 @@ CONNECTION_STRING = "sqlite:///trackedTweets.db"
 CSV_NAME = "trackedTweets.csv"
 TABLE_NAME = "trackedTweets"
 
+# What are the values in this topic that you want to compare
 NEG_TOPICS = {'arthur': 0, 'chanelle': 0, 'charlotte': 0, 'deborah': 0, 'ellie': 0, 'hannah': 0, 'imran': 0, 'kayleigh': 0, 'kieran': 0, 'lotan': 0, 'mandy': 0, 'raph': 0, 'rebecca': 0, 'sukhvinder': 0, 'tom': 0}
 
 POS_TOPICS = {'arthur': 0, 'chanelle': 0, 'charlotte': 0, 'deborah': 0, 'ellie': 0, 'hannah': 0, 'imran': 0, 'kayleigh': 0, 'kieran': 0, 'lotan': 0, 'mandy': 0, 'raph': 0, 'rebecca': 0, 'sukhvinder': 0, 'tom': 0}
